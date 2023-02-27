cbb259d814ae1da62531ea4b16132985c0bf7b22
Le Manh Duong (VTI.D5) <duong.lemanh@vti.com.vn>
2023-02-27 14:53:15 +0700

    init time count-down

---
+++ src/Component/TimeCountdown/TimeCountdown.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+export default function TimeCountdown() {
+  return (
+    <div className="back-ground">
+      <div className="time-wrapper">
+        <div className="main-content"></div>
+      </div>
+    </div>
+  );
+}
